29b06e72df6eb004210dcf1ff3aeeb11de74ebff
TangLongbin <1982917081@qq.com>
2022-08-03 18:04:26 +0800

    Update！

--- part-1/parse_the_data.py
+++ part-1/parse_the_data.py
@@ -1,11 +1,14 @@
 import json
 import os
 
-
-# 数据读写文件路径
+# 原始数据文件夹目录
 DataPath = "data/document_parses"
-OutTemplateFile = "parsed_data_texts/text"
+# 输出文件目录+文件名（目录/文件名）
+OutTemplateFile = "parsed_data_title&text/text"
 
+# 该程序会提取数据文件夹中所有json文件的title和text项
+# 最后修改时间：2022/8/3/17：57
+# 最后修改者：唐隆斌
 
 
 # 递归遍历一个字典的所有键值对
@@ -17,7 +20,7 @@ def dfs_dict(dict_now, OutPutFile):
     keys = dict_now.keys()
     for key in keys:
         # 读取文件中所有“text”的内容（可根据需求替换）
-        if key == "text" and len(dict_now[key]) > 20:
+        if (key == "text" or key == "title") and len(dict_now[key]) > 20:
             OutPutFile.write(dict_now[key] + '\n')
         else:
             dfs_dict(dict_now[key], OutPutFile)
@@ -54,12 +57,14 @@ def main():
     # 获取所有文件路径并依次遍历
     FilePaths = GetFilePath(DataPath)
     cnt = 0
+    total = len(FilePaths)
     for FilePath in FilePaths:
         OutPutPath = OutTemplateFile + "_" + str(cnt) + ".txt"
         OutPutFile = open(OutPutPath, "w")
         GetValueFromFile(FilePath, OutPutFile)
+        # 输出程序完成百分比
         print("\r",end="")
-        print(cnt, end="")
+        print(100* cnt//total, end="%")
         cnt += 1
         OutPutFile.close()
     
